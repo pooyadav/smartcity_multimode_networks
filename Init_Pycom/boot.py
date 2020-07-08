6b723939ff09ba1b3631f6b62acf27c97e8a905c
root <root@node-0004.replicate.local>
2020-07-08 21:42:27 +0000

    Linted code

--- Init_Pycom/boot.py
+++ Init_Pycom/boot.py
@@ -1,12 +1,14 @@
+""" Function definition for connecting different networks"""
+import time
 import machine
 from network import WLAN
-import time
 
-#The code is taken from https://docs.pycom.io/chapter/tutorials/all/wlan.html. from the section Connecting to a Router.
+#The code is taken from https://docs.pycom.io/chapter/tutorials/all/wlan.html.
 wlan = WLAN(mode=WLAN.STA)
 
-# A function has been defined to be called in the main file to make it shorter and more efficient
-def connect_wifi(ssid,passwifi):
+# A function has been defined to be called in the main file to make it shorter
+def connect_wifi(ssid, passwifi):
+    """ Connect WiFi using provided ssid, password"""
 
     print("Trying to connect to " + ssid)
     # wlan.antenna(WLAN.EXT_ANT)
@@ -22,61 +24,64 @@ def connect_wifi(ssid,passwifi):
                 machine.idle() # save power while waiting
             print('WLAN connection succeeded!')
             return True
-    print ("network not found")
-    connect_wifi(ssid,passwifi)
+    print("network not found")
+    connect_wifi(ssid, passwifi)
 
 def connect_sigfox():
+    """ Connect to SigFox and return sigfox and socket"""
 
-    sigfox = Sigfox(mode=Sigfox.SIGFOX, rcz=Sigfox.RCZ1)
+    obj_sigfox = Sigfox(mode=Sigfox.SIGFOX, rcz=Sigfox.RCZ1)
 # Create a Sigfox socket
-    s = socket.socket(socket.AF_SIGFOX, socket.SOCK_RAW)
+    sock_sigfox = socket.socket(socket.AF_SIGFOX, socket.SOCK_RAW)
     print("Hello3")
 # make the socket blocking
-    s.setblocking(True)
+    sock_sigfox.setblocking(True)
 # Configure it as uplink
-    s.setsockopt(socket.SOL_SIGFOX, socket.SO_RX, False)
+    sock_sigfox.setsockopt(socket.SOL_SIGFOX, socket.SO_RX, False)
 # send some data
-    s.send("Hello SigFox")
-    print ("Message sent on Sigfox")
-    return sigfox, s
+    sock_sigfox.send("Hello SigFox")
+    print("Message sent on Sigfox")
+    return obj_sigfox, sock_sigfox
 
 def connect_lora_otaa():
-    lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.EU868)
-    lora.join(activation=LoRa.OTAA, auth=(app_eui, app_key), timeout=0)
+    """ Connect to LoRa and return lora and socket via OTAA auth method"""
+    obj_lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.EU868)
+    obj_lora.join(activation=LoRa.OTAA, auth=(app_eui, app_key), timeout=0)
 
     # wait until the module has joined the network
-    while not lora.has_joined():
+    while not obj_lora.has_joined():
         time.sleep(2.5)
         print('Not yet joined...')
 
-    print ("Finally Joined")
+    print("Finally Joined")
 
 # Print Stats
-    print("Lora.bandwidth is " + lora.Bandwidth())
-    print("Lora.sf is " + lora.sf())
-    print("lora.coding_rate is " + lora.coding_rate())
+    print("Lora.bandwidth is " + obj_lora.Bandwidth())
+    print("Lora.sf is " + obj_lora.sf())
+    print("lora.coding_rate is " + obj_lora.coding_rate())
 # create a LoRa socket
-    s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+    sock_lora = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 
 # set the LoRaWAN data rate
-    s.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
+    sock_lora.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
 
 # make the socket blocking
 # (waits for the data to be sent and for the 2 receive windows to expire)
-    s.setblocking(True)
+    sock_lora.setblocking(True)
 # send some data
-    s.send(bytes([0x01, 0x02, 0x03]))
+    sock_lora.send(bytes([0x01, 0x02, 0x03]))
 # make the socket non-blocking
 # (because if there's no data received it will block forever...)
-    s.setblocking(False)
+    sock_lora.setblocking(False)
 
 # get any data received (if any...)
-    data = s.recv(64)
+    data = sock_lora.recv(64)
     print(data)
-    return lora, s
+    return obj_lora, sock_lora
 
 
 def connect_lora_abp():
+    """ Connect to LoRa and return lora and socket via ABP auth method"""
     lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.EU868)
 
 # create an ABP authentication params
@@ -108,53 +113,56 @@ def connect_lora_abp():
     data = s.recv(64)
     print(data)
 
-def connect_UART():
+def connect_uart():
+    """ Connect to UART Pins P3/P4 and send data to the cloud"""
     uart = UART(1, baudrate=115200)
     uart.init(115200, bits=8, parity=None, stop=1)
 #    send_mqtt("HelloWorld")
     while True:
         try:
-            if (uart.any() != 0):
+            if uart.any() != 0:
                 uart_msg = uart.readline()
-                if uart_msg != None:
+                if uart_msg is not None:
 #                    send_mqtt(uart_msg)
                     check_connection()
                     uart.write("Data sent\n")
         except:
-            print ("Keyboard Interrupt")
+            print("Keyboard Interrupt")
             raise
             break
 
 def connect_nbiot():
-    lte = LTE()
-    lte.attach(band=20, apn="nb.inetd.gdsp")
+    """ Connect to NB-IoT network"""
+    obj_lte = LTE()
+    obj_lte.attach(band=20, apn="nb.inetd.gdsp")
     while not lte.isattached():
         print("LTE: Not attached yet")
         time.sleep(0.25)
     print("LTE: Attached")
-    lte.connect()       # start a data session and obtain an IP address
-    while not lte.isconnected():
+    obj_lte.connect()       # start a data session and obtain an IP address
+    while not obj_lte.isconnected():
         print("LTE: Not connected yet")
         time.sleep(0.25)
     print("LTE: Connected")
 
 def check_connection():
+    """ Check which networks are connected and send data via that network """
     i = 1
     while i < 6:
 #        check if wlan is connected
-#       if connected great! If not check if nb-iot is connected, if not check lora is connected, if not SigFox
+#       if connected great! If not check if nb-iot, lora, sigfox
         connected_wifi = wlan.isconnected()
         connected_lte = lte.isconnected()
         connected_lora = lora.has_joined()
-        if connected_wifi == True:
+        if connected_wifi:
             ## Send data using WiFi
             print("Data on WiFi")
-        elif connected_lte == True:
+        elif connected_lte:
             ## Send data using lte
             print("Data on LTE")
-        elif connected_lora != True:
+        elif connected_lora:
             ## Send data using lora
             print("Data on Lora")
         else:
             # Send only important data on SigFox (12bytes)
-            print ("Data sent on SigFox")
+            print("Data sent on SigFox")
--- Init_Pycom/main.py
+++ Init_Pycom/main.py
@@ -1,25 +1,28 @@
-import pycom
+""" Main program for Multi-modal network POC """
 import time
 import struct
-from network import Sigfox
+import socket
+import pycom
 from umqtt import MQTTClient
 from machine import UART
 from network import LoRa
-import socket
+from network import Sigfox
 import ubinascii
 
 # Wifi_Creds
-wifi_ssid = "BTHub6-WK6Q-LS"
-wifi_pass = "9XGDxfLnPvEq"
+WIFI_SSID = "BTHub6-WK6Q-LS"
+WIFI_PASS = "9XGDxfLnPvEq"
 # Lora OTAA Key
 # create an OTAA authentication parameters
-app_eui = ubinascii.unhexlify('70B3D57ED0030B7E')
-app_key = ubinascii.unhexlify('02BEBE90D3FF4C1C26EBD71DDA585214')
+APP_EUI = ubinascii.unhexlify('70B3D57ED0030B7E')
+APP_KEY = ubinascii.unhexlify('02BEBE90D3FF4C1C26EBD71DDA585214')
 
 def sub_cb(topic, msg):
-   print(msg)
+    """ Function to Not sure Print Message """
+    print(msg)
 
 def send_mqtt(mqtt_msg):
+    """ Function to Send MQTT Message """
     client = MQTTClient("PyCom-LoPy", "node-0003", port=1884)
     client.set_callback(sub_cb)
     client.connect()
@@ -27,9 +30,9 @@ def send_mqtt(mqtt_msg):
     client.publish(topic="HelloWorld/HelloUART", msg=mqtt_msg)
     client.check_msg()
 
-
 def main():
-    connect_wifi(wifi_ssid, wifi_pass)
+    """ Main function currently make calls to connect all networks and UART """
+    connect_wifi(WIFI_SSID, WIFI_PASS)
     lora, s_lora = connect_lora_otaa()
     sigfox, s_sigfox = connect_sigfox()
     connect_UART()
